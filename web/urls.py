6029e7f0bd784cb960032c8d67b29d4fbf3f6022
gonzaloparod1 <gonzalo.parodi@gmail.com>
2024-08-07 23:41:56 -0400

    modified hito 2 version final

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,7 @@
-
 from django.urls import path
 from . import views
 
-# /z/
+
 urlpatterns = [
     path('', views.inicio),
     path('acerca/', views.acerca)
--- web/views.py
+++ web/views.py
@@ -4,13 +4,9 @@ from django.shortcuts import render
 
 def inicio(req): 
     context = {
-        "mensaje": "APP WEBZ",
-        "productos": [{"name": "tv", "url":"vvv"},{"name": "celu", "url":"www"},{"name": "mesa", "url":"zzz"}]
-    }
+        "mensaje": "Bienvenido Cliente",
+            }
     return render(req, 'index.html', context)
 
 def acerca(req): 
-    context = {
-        "mensaje": "Soy un dev que ...",
-    }
-    return render(req, 'acerca.html', context)
+    return render(req, 'about.html', {})
